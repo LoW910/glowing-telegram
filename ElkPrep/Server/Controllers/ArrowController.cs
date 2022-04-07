6722b0e0dbafc3d6e444b60954cc3dcbe47e5be1
Daniel Soots <danielsoots@icloud.com>
2022-04-06 21:56:22 -0400

    Created Arrow controller interface and service.

---
+++ ElkPrep/Server/Controllers/ArrowController.cs
@@ -0,0 +1,60 @@
+﻿using ElkPrep.Server.Interfaces;
+using ElkPrep.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElkPrep.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArrowController : ControllerBase
+    {
+        private readonly IArrow _IArrow;
+
+        public ArrowController(IArrow iArrow)
+        {
+            _IArrow = iArrow;
+        }
+
+
+        // Get list of all arrows
+        [HttpGet("")]
+        public async Task<List<Arrow>> GetArrows()
+        {
+            return await Task.FromResult(_IArrow.GetAllArrows());
+        }
+
+        // Get Arrow by id
+        [HttpGet("{id}")]
+        public ActionResult<Arrow> GetArrow(int id)
+        {
+            Arrow arrow = _IArrow.GetArrow(id);
+            if (arrow != null)
+            {
+                return arrow;
+            }
+            return NotFound();
+        }
+
+        // Create Arrow
+        [HttpPost("add")]
+        public void CreateArrow(Arrow arrow)
+        {
+            _IArrow.AddArrow(arrow);
+        }
+
+        // Update Arrow
+        [HttpPut("update/{id}")]
+        public void UpdateArrow(Arrow arrow)
+        {
+            _IArrow.UpdateArrow(arrow);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteArrow(int id)
+        {
+            _IArrow.DeleteArrow(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ ElkPrep/Server/Services/ArrowService.cs
@@ -0,0 +1,97 @@
+﻿using ElkPrep.Server.DAL;
+using ElkPrep.Server.Interfaces;
+using ElkPrep.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElkPrep.Server.Services
+{
+    public class ArrowService : IArrow
+    {
+
+        readonly ElkPrepContext _dbContext = new();
+
+        public ArrowService(ElkPrepContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddArrow(Arrow arrow)
+        {
+            try
+            {
+                arrow.DateCreated = DateTime.Now;
+                _dbContext.Arrows.Add(arrow);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Add User");
+            }
+        }
+
+        public void DeleteArrow(int id)
+        {
+            try
+            {
+                Arrow? arrow = _dbContext.Arrows.Find(id);
+                if (arrow != null)
+                {
+                    _dbContext.Arrows.Remove(arrow);
+                    _dbContext.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Remove Arrow");
+            }
+        }
+
+        public List<Arrow> GetAllArrows()
+        {
+            try
+            {
+                return _dbContext.Arrows.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Get all Arrows");
+                return null;
+            }
+        }
+
+        public Arrow GetArrow(int id)
+        {
+            try
+            {
+                Arrow? arrow = _dbContext.Arrows.Find(id);
+                if(arrow != null)
+                {
+                    return arrow;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Get Arrow");
+                return null;
+            }
+        }
+
+        public void UpdateArrow(Arrow arrow)
+        {
+            try
+            {
+                arrow.DateUpdated = DateTime.Now;
+                _dbContext.Entry(arrow).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Failed to Update Arrow");
+            }
+        }
+    }
+}
